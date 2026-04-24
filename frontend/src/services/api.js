61fdc3a451df5f964e958245cde11981ca29152d
Ashok Kumar <ashokkumar885318@gmail.com>
2026-04-24 18:13:20 +0530

    fix api url and signup issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
-
+const API_URL =  "https://early-heart-attack-detection-system.onrender.com";
 const api = axios.create({
   baseURL: API_URL,
 });
